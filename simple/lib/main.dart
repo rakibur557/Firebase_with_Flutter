0ad1e1bd546aa137b1529b2dbd5f95ebe522c383
Rakibur Rahman <rakiburrahman.cse.eng@gmail.com>
2022-04-09 15:41:38 +0600

    updat pubspec

--- simple/lib/main.dart
+++ simple/lib/main.dart
@@ -2,7 +2,8 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:simple/pages/homeScreen.dart';
-
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
 import 'services/services.dart';
 
 void main() => runApp(const MyApp());
@@ -17,9 +18,16 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/': (ctx) =>HomeScreen(),
+        //'/': (ctx) =>HomeScreen(),
         services.path: (ctx)=>services(),
       },
+      home: AnimatedSplashScreen(
+          duration: 3000,
+          splash: Image.network('assets/images/splashlogo.png'),
+          nextScreen: HomeScreen(),
+          splashTransition: SplashTransition.fadeTransition,
+          pageTransitionType: PageTransitionType.scale,
+          backgroundColor: Colors.white),
     );
   }
 }
