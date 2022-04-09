bb3e762227c3aa285c0c88cbd344690dba19901e
Rakibur Rahman <rakiburrahman.cse.eng@gmail.com>
2022-04-09 17:52:07 +0600

    login page1

--- simple/lib/pages/homeScreen.dart
+++ simple/lib/pages/homeScreen.dart
@@ -10,7 +10,29 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+                  children: [
+                    Image.asset('assets/images/login.png'),
+                    SizedBox(height: 20,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: TextField(
+                        cursorColor: Colors.black,
 
+                        decoration: InputDecoration(
+                          hintText: 'Enter your Email',
+                          prefixIcon: Icon(Icons.mail),
+                        ),
+
+                      ),
+                    ),
+                  ],
+                ),
+          ),
+        ),
     );
   }
 }
