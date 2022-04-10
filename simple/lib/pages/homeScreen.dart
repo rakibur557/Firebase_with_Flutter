e3c867165d422423c29fe6e7232fe2eb867a08a0
Rakibur Rahman <rakiburrahman.cse.eng@gmail.com>
2022-04-10 21:04:37 +0600

    delete

--- simple/lib/pages/homeScreen.dart
+++ simple/lib/pages/homeScreen.dart
@@ -1,4 +1,7 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -11,28 +14,48 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-                  children: [
-                    Image.asset('assets/images/login.png'),
-                    SizedBox(height: 20,),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 30),
-                      child: TextField(
-                        cursorColor: Colors.black,
-
-                        decoration: InputDecoration(
-                          hintText: 'Enter your Email',
-                          prefixIcon: Icon(Icons.mail),
-                        ),
-
-                      ),
-                    ),
-                  ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset('assets/images/login.png'),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                'Log In',
+                style: TextStyle(
+                  fontSize: 20,
                 ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter your Email',
+                    prefixIcon: Icon(Icons.mail),
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your Password',
+                    prefixIcon: Icon(Icons.mail),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+            ],
           ),
         ),
+      ),
     );
   }
 }
