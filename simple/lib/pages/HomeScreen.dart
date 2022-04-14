a388942ac7ced9cba898fc0da70e3c24f37d7e42
Rakibur Rahman <rakiburrahman.cse.eng@gmail.com>
2022-04-14 09:34:32 +0600

    image picker

--- simple/lib/pages/HomeScreen.dart
+++ simple/lib/pages/HomeScreen.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:simple/services/googleServiceProvider.dart';
@@ -6,7 +7,7 @@ import 'LoginScreen.dart';
 class HomeScreen extends StatefulWidget {
   static const String path = "HomeScreen";
 
-  HomeScreen({Key? key,  this.email, this.name, this.photoUrl})
+  HomeScreen({Key? key, this.email, this.name, this.photoUrl})
       : super(key: key);
 
   final email, name, photoUrl;
@@ -17,6 +18,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   googleServiceProvider googleService = googleServiceProvider();
+  File? _pickedImage;
 
   @override
   Widget build(BuildContext context) {
@@ -26,28 +28,118 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
       ),
       body: Center(
-        child: ListTile(
-          leading: CircleAvatar(
-            backgroundImage: NetworkImage(widget.photoUrl),
-          ),
-          title: Text(widget.name),
-          subtitle: Text(widget.email),
-          trailing: ElevatedButton(
-            onPressed: () async {
-              googleService.logout();
-              setState(() {
-                Navigator.pushReplacement(
-                  context,
-                  CupertinoPageRoute(
-                    builder: (context) => LoginScreen(),
+          child: Column(
+        children: [
+          //SizedBox(height: 10,),
+          Stack(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 30, horizontal: 30),
+                child: CircleAvatar(
+                  radius: 70,
+                  backgroundColor: Colors.blue,
+                  child: CircleAvatar(
+                    radius: 65,
+                    backgroundColor: Colors.white,
+                    backgroundImage:
+                        _pickedImage == null ? null : FileImage(_pickedImage!),
                   ),
-                );
-              });
-            },
-            child: Text('LogOut'),
+                ),
+              ),
+              Positioned(
+                  top: 115,
+                  left: 105,
+                  child: RawMaterialButton(
+                    elevation: 10,
+                    fillColor: Colors.blue,
+                    child: Icon(Icons.add_a_photo, color: Colors.white),
+                    shape: CircleBorder(),
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text(
+                                'Choose an option',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                    color: Colors.blue),
+                              ),
+                              content: SingleChildScrollView(
+                                child: ListBody(
+                                  children: [
+                                    InkWell(
+                                      splashColor: Colors.blue,
+                                      onTap: () {},
+                                      child: Row(
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Icon(Icons.camera_alt),
+                                          ),
+                                          Text(
+                                            'Camera',
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+
+                                    InkWell(
+                                      splashColor: Colors.blue,
+                                      onTap: () {},
+                                      child: Row(
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Icon(Icons.image),
+                                          ),
+                                          Text(
+                                            'Gallery',
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+
+                                  ],
+                                ),
+                              ),
+                            );
+                          });
+                    },
+                  )),
+            ],
           ),
-        ),
-      ),
+
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(widget.photoUrl),
+            ),
+            title: Text(widget.name),
+            subtitle: Text(widget.email),
+            trailing: ElevatedButton(
+              onPressed: () async {
+                googleService.logout();
+                setState(() {
+                  Navigator.pushReplacement(
+                    context,
+                    CupertinoPageRoute(
+                      builder: (context) => LoginScreen(),
+                    ),
+                  );
+                });
+              },
+              child: Text('LogOut'),
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
